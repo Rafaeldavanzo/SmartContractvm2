d28a94493a9923cca8e25a7b0d9b4c91e2a67bdf
Rafaeldavanzo <85182286+Rafaeldavanzo@users.noreply.github.com>
2021-12-12 10:45:32 +0000

    Update contract.py

--- contract.py
+++ contract.py
@@ -1,1 +1,54 @@
-print ("Hello world")
+import time
+import json
+import web3
+from eth_account import Account
+from web3.auto import w3
+from web3.providers.websocket import WebsocketProvider
+from web3 import Web3
+from solc import compile_standard
+
+with open("contract.sol") as c:
+  contractText=c.read()
+with open(".pk") as pkfile:
+  privateKey=pkfile.read()
+with open(".infura") as infurafile:
+  infuraKey=infurafile.read()
+
+compiled_sol = compile_standard({
+    "language": "Solidity",
+    "sources": {
+        "Greeter.sol": {
+            "content": contractText
+        }
+    },
+    "settings":
+        {
+            "outputSelection": {
+                "*": {
+                    "*": [
+                        "metadata", "evm.bytecode"
+                        , "evm.bytecode.sourceMap"
+                    ]
+                }
+            }
+        }
+})
+bytecode = compiled_sol['contracts']['Greeter.sol']['Greeter']['evm']['bytecode']['object']
+abi = json.loads(compiled_sol['contracts']['Greeter.sol']['Greeter']['metadata'])['output']['abi']
+W3 = Web3(WebsocketProvider('wss://ropsten.infura.io/ws/v3/%s'%infuraKey))
+account1=Account.from_key(privateKey);
+address1=account1.address
+Greeter = W3.eth.contract(abi=abi, bytecode=bytecode)
+
+nonce = W3.eth.getTransactionCount(address1)
+#diagnostics
+#print(nonce)
+# Submit the transaction that deploys the contract
+tx_dict = Greeter.constructor().buildTransaction({
+  'chainId': 3,
+  'gas': 1400000,
+  'gasPrice': w3.toWei('40', 'gwei'),
+  'nonce': nonce,
+  'from':address1
+})
+

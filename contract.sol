a524b6ad72b848b638d4905b39cf0dfd0ba86e83
Rafaeldavanzo <85182286+Rafaeldavanzo@users.noreply.github.com>
2021-12-22 12:38:08 +0000

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -14,4 +14,27 @@ contract Greeter {
   function greet() view public returns (string memory) {
     return greeting;
   }
+  
+  
+   address public minter;
+
+   mapping (address => uint) public balances;
+   
+   event Sent(address from, address to, uint amount);
+
+   constructor() public {
+      minter = msg.sender;
+      }
+   function mint(address receiver, uint amount) public {
+      require(msg.sender == minter);
+      require(amount < 1e60);
+      balances[receiver] += amount;
+   }
+
+   function send(address received, uint amount) public {
+      require(amount <= balances[msg.sender], "Insufficient balance.");
+      balances[msg.sender] -= amount;
+      balances[receiver] += amount;
+      emit Sent(msg.sender, receiver, amount);
+   }
 }

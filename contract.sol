53a7c8d9315f6654bfd4ca3ece5d3d83319bbb99
Rafaeldavanzo <85182286+Rafaeldavanzo@users.noreply.github.com>
2021-12-22 19:10:15 +0000

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -5,22 +5,13 @@ contract Greeter {
 
   constructor() public {
     greeting = 'Hello';
-    wint _id;
-    string -firstname;
-    string _lastName;
   }
-  functions addPerson(String memory _firstName, string memory _LastName) public {
-    peopleCount += 1;
-    people[peopleCount] = Person(peopleCount, _firstName, -lastName);
-  }
+
   function setGreeting(string memory _greeting) public {
     greeting = _greeting;
   }
 
   function greet() view public returns (string memory) {
     return greeting;
-  
   }
 }
-
-
